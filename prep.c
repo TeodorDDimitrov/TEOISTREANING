fa1eefced1ef63c2e22e54cc22237091acef138a
Teodor Dimitrov <teo.dim.dimitrov@gmail.com>
2025-01-17 21:03:36 +0200

    ang

--- prep.c
+++ prep.c
@@ -1,13 +1,5 @@
 #include<stdio.h>
 void main(){
-<<<<<<< HEAD
 	printf("Hello Тео");
-	printf("Hello abc");
-=======
-<<<<<<< HEAD
-	printf("Hello Teo");
-=======
-	printf("Hello Тео");
->>>>>>> c1e6028b152aac29b4567134c14cc4f27fdb6461
->>>>>>> main
+	printf("Hello ang");
 }
